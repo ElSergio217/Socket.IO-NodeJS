7db96193f56e46d501fa5a7661a76ca2fa312d8c
Sergio <Sergioa.garcia217@gmail.com>
2015-10-24 22:00:46 -0400

    Heroku updated

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
-	var port=Number(process.env.PORT||3000)
-server.listen(port);
+	
+server.listen(3000);
 
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/index.html');
